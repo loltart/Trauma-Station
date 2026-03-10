ea400b39566a6bfeaf6b3049553a6a362bd447ec
Tayrtahn <tayrtahn@gmail.com>
2026-03-10 17:25:45 -0400

    Update `NodeHelpers` and `Node.GetReachableNodes` to use `Entity<TransformComponent>` and `Entity<MapGridComponent>` (#37734)
    * Add overloads for NodeHelpers methods, obsolete old ones
    
    * Pass in SharedMapSystem instead of IEntityManager
    
    * Change GetReachableNodes signature, deal with aftermath
    
    * Allow non-grid nodes
    
    * Change GetReachableNodes to also take Entity<TransformComponent>
    
    * Remove vestigial ReSharper hint
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>
    
    * Formatting
    
    ---------
    
    Co-authored-by: TemporalOroboros <TemporalOroboros@gmail.com>

--- Content.Server/Power/Nodes/CableDeviceNode.cs
+++ Content.Server/Power/Nodes/CableDeviceNode.cs
@@ -1,18 +1,3 @@
-// SPDX-FileCopyrightText: 2021 20kdc <asdd2808@gmail.com>
-// SPDX-FileCopyrightText: 2021 Caj Jones <delle.caj@gmail.com>
-// SPDX-FileCopyrightText: 2021 Vera Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2021 Vera Aguilera Puerto <gradientvera@outlook.com>
-// SPDX-FileCopyrightText: 2022 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2022 mirrorcult <lunarautomaton6@gmail.com>
-// SPDX-FileCopyrightText: 2022 wrexbe <81056464+wrexbe@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-//
-// SPDX-License-Identifier: MIT
-
-using Content.Server.NodeContainer;
 using Content.Server.NodeContainer.EntitySystems;
 using Content.Server.NodeContainer.Nodes;
 using Content.Shared.NodeContainer;
@@ -45,18 +30,20 @@ namespace Content.Server.Power.Nodes
             return base.Connectable(entMan, xform);
         }
 
-        public override IEnumerable<Node> GetReachableNodes(TransformComponent xform,
+        public override IEnumerable<Node> GetReachableNodes(
+            Entity<TransformComponent> xform,
             EntityQuery<NodeContainerComponent> nodeQuery,
             EntityQuery<TransformComponent> xformQuery,
-            MapGridComponent? grid,
+            Entity<MapGridComponent>? grid,
             IEntityManager entMan)
         {
-            if (!xform.Anchored || grid == null)
+            if (!xform.Comp.Anchored || grid is not { } gridEnt)
                 yield break;
 
-            var gridIndex = grid.TileIndicesFor(xform.Coordinates);
+            var mapSystem = entMan.System<SharedMapSystem>();
+            var gridIndex = mapSystem.TileIndicesFor(gridEnt, xform.Comp.Coordinates);
 
-            foreach (var node in NodeHelpers.GetNodesInTile(nodeQuery, grid, gridIndex))
+            foreach (var node in NodeHelpers.GetNodesInTile(nodeQuery, gridEnt, gridIndex, mapSystem))
             {
                 if (node is CableNode)
                     yield return node;
